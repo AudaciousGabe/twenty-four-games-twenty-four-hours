ac2a70942765bbda7efa8c839a9246718cc58e80
AudaciousGabe <audaciousgabe@gmail.com>
2026-03-02 02:38:31 -0500

    mazes progress

--- games/4_maze/four_mazes.gd
+++ games/4_maze/four_mazes.gd
@@ -2,16 +2,25 @@ extends Control
 
 
 
-@export var first_maze_texture_rect: TextureRect
 @export var cursor_sprite_2d: Sprite2D
+@export var banned_text_rich_text_label: RichTextLabel
 
+@export var maze_texture_rect: TextureRect
+@export var maze_texture_rect_2: TextureRect
+@export var maze_texture_rect_3: TextureRect
+@export var maze_texture_rect_4: TextureRect
+
+@export var death_audio_stream_player: AudioStreamPlayer
 
 var current_maze_index: int = 0
 
 var maze_attempt_started: bool = false
 
 @onready var maze_texture_rects: Array[TextureRect] = [
-	first_maze_texture_rect,
+	maze_texture_rect,
+	maze_texture_rect_2,
+	maze_texture_rect_3,
+	maze_texture_rect_4,
 ]
 
 
@@ -33,10 +42,13 @@ func is_mouse_position_color_transparent(mouse_position: Vector2) -> bool:
 	var current_maze_image: Image = first_current_maze_texture.get_image()
 	var mouse_color: Color = current_maze_image.get_pixelv(mouse_position)
 	
-	if mouse_color == Color.TRANSPARENT:
+	print(mouse_color)
+	
+	if mouse_color.a == 0:
 		return true
 
 	return false
+
 
 func get_current_maze() -> TextureRect:
 	return maze_texture_rects[current_maze_index]
@@ -44,5 +56,13 @@ func get_current_maze() -> TextureRect:
 
 func _on_maze_texture_rect_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		print("Position: ", event.position)
 		print(is_mouse_position_color_transparent(event.position))
+		
+		if not is_mouse_position_color_transparent(event.position):
+			cursor_sprite_2d.hide()
+			banned_text_rich_text_label.show()
+
+
+func _on_reset_button_pressed() -> void:
+	cursor_sprite_2d.show()
+	banned_text_rich_text_label.hide()
